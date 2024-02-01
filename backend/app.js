f62e4597076cf3a4f08d91668f5d8fcb66c45576
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-02-01 09:36:58 +0300

    delete headers

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,23 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import express from 'express';
-import mongoose from 'mongoose';
-import { errors } from 'celebrate';
-import router from './routes/index.js';
-import handleCenterError from './middlewares/centerError.js';
-import NotFoundError from './errors/NotFoundError.js';
-import { requestLogger, errorLogger } from './middlewares/logger.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+import { errors } from "celebrate";
+import router from "./routes/index.js";
+import handleCenterError from "./middlewares/centerError.js";
+import NotFoundError from "./errors/NotFoundError.js";
+import { requestLogger, errorLogger } from "./middlewares/logger.js";
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 
 app.use(requestLogger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 
 app.use(router);
 
@@ -23,7 +25,9 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
+app.use("*", (req, res, next) =>
+  next(new NotFoundError("Страница не найдена"))
+);
 
 app.use(handleCenterError);
 
